7178d64b1e7eed96d051f2efd31f99926233a22a
Jose Juan Navarro <jjnavarromillan.staf@gmail.com>
2018-01-15 13:18:07 -0800

    First Commit

---
+++ WpfReportDemo/DataSet1.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfReportDemo
+{
+}
+
+namespace WpfReportDemo
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
+namespace WpfReportDemo {
+    
+    
+    public partial class DataSet1 {
+    }
+}
--- WpfReportDemo/MainWindow.xaml.cs
+++ WpfReportDemo/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Reporting.WinForms;
 
 namespace WpfReportDemo
 {
@@ -23,6 +26,26 @@ namespace WpfReportDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReportViewerDemo.Reset();
+            DataTable dt = GetData();
+            ReportDataSource ds = new ReportDataSource("DataSet1",dt);
+            ReportViewerDemo.LocalReport.DataSources.Add(ds);
+            ReportViewerDemo.LocalReport.ReportEmbeddedResource = "WpfReportDemo.Report1.rdlc";
+            ReportViewerDemo.RefreshReport();
+        }
+        private DataTable GetData() {
+            string connStr = @"data source=.\SQLEXPRESS;initial catalog=phv-berry;integrated security=True";
+            DataTable dt = new DataTable();
+            using (SqlConnection cn = new SqlConnection(connStr)) {
+                SqlCommand cmd = new SqlCommand("Select CompanyID,Name from Company", cn);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(dt);
+            }
+            return dt;
         }
     }
 }
